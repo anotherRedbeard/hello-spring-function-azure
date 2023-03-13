0e182196a61f166d854c3d33afd2f01edd70f623
anotherRedbeard <andrewredman@redbeard-mbp.lan>
2023-03-12 20:53:42 -0500

    working with uppercase function hardcoded

--- src/main/java/com/example/HelloFunction.java
+++ src/main/java/com/example/HelloFunction.java
@@ -2,8 +2,8 @@ package com.example;
 
 import com.example.model.Greeting;
 import com.example.model.User;
+
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
--- src/main/java/com/example/HelloHandler.java
+++ src/main/java/com/example/HelloHandler.java
@@ -6,26 +6,36 @@ import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 public class HelloHandler extends AzureSpringBootRequestHandler<User, Greeting> {
 
-    @FunctionName("hello")
-    public HttpResponseMessage execute(
-            @HttpTrigger(name = "request", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
-            ExecutionContext context) {
-        User user = request.getBody()
-                .filter((u -> u.getName() != null))
-                .orElseGet(() -> new User(
-                        request.getQueryParameters()
-                                .getOrDefault("name", "world")));
-        context.getLogger().info("Greeting user name: " + user.getName());
-        return request
-                .createResponseBuilder(HttpStatus.OK)
-                .body(handleRequest(user, context))
-                .header("Content-Type", "application/json")
-                .build();
-    }
+        //@Autowired
+        //private Function<String, String> uppercase;
+        
+        //pulling in uppercase function from HelloFunction.java without using @Autowired
+        private final Function<String, String> uppercase = payload -> payload.toUpperCase();
+        
+        @FunctionName("hello")
+        public HttpResponseMessage execute(
+                @HttpTrigger(name = "request", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
+                ExecutionContext context) {
+                User user = request.getBody()
+                        .filter((u -> u.getName() != null))
+                        .orElseGet(() -> new User(
+                                request.getQueryParameters()
+                                        .getOrDefault("name", "world")));
+                context.getLogger().info("Greeting user name: " + user.getName());
+                context.getLogger().info("UPPERCASED Greeting user name: " + uppercase.apply(user.getName()));
+                return request
+                        .createResponseBuilder(HttpStatus.OK)
+                        .body(handleRequest(user, context))
+                        .header("Content-Type", "application/json")
+                        .build();
+        }
 }
