05b4437f31e85b1cbf7d9a247014e1f8bb1b733e
anotherRedbeard <andrewredman@redbeard-mbp.lan>
2023-03-28 14:53:56 -0500

    simulate abandon lock by throwing exception

--- src/main/java/com/example/HelloHandler.java
+++ src/main/java/com/example/HelloHandler.java
@@ -8,6 +8,7 @@ import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 import com.microsoft.azure.functions.annotation.ServiceBusQueueTrigger;
@@ -38,10 +39,20 @@ public class HelloHandler {
         @FunctionName("sbprocessor")
         public void serviceBusProcess(
                 @ServiceBusQueueTrigger(name = "msg", queueName = "demo-queue", connection = "SBConnectionString") String message, 
-            final ExecutionContext context) {
+                final ExecutionContext context,
+                @BindingName("DeliveryCount") int deliveryCount,
+                @BindingName("MessageId") String messageId) {
 
                 Logger logger = context.getLogger();
                 String adjustedMessage = this.uppercase.apply(message);
-                logger.info(adjustedMessage);
+
+                logger.info("Delivery count: " + deliveryCount);
+                logger.info("Message body: " + adjustedMessage);
+                logger.info("Message id: " + messageId);
+
+                if (message.equals("throw exception") && deliveryCount <= 3)
+                {
+                        throw new RuntimeException("Error processing message");
+                }
         }
 }
